4b9784f7279927e2340d979afe4b90354b3230b6
Jacob Diehl <jakediehl17@gmail.com>
2025-07-31 15:23:07 -0600

    Finish the placement of images and buttons for the linking site

--- app/page.js
+++ app/page.js
@@ -1,95 +1,31 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <img
+        src="/images/Jessie_Krebs_Color.png"
+        alt="Jessie Krebs"
+        className={`${styles.center_self_horizontally} ${styles.site_connection_logo}`}
+      />
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <div className={styles.background_color_steel}>
+        <div className={styles.horizontaly_divide_two_divs}>
+          <div className={styles.image_button_container}>
+            <img src="/images/Logo_Jessie_gmail.png" alt="Custom Survival Course" />
+              <button className={`${styles.position_button_under_prev} ${styles.button_for_site_linking_form} ${styles.background_color_light_purple}`}>
+                Private Custom Courses by Jessie Krebs
+              </button>
+          </div>
+          <div className={styles.image_button_container}>
+            <img src="/images/Custom_Survival_Course.png" alt="Women's Survival Course" />
+              <button className={`${styles.position_button_under_prev} ${styles.button_for_site_linking_form} ${styles.background_color_light_green}`}>
+                Women's Survival School * by Jessie Krebs
+              </button>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
+
