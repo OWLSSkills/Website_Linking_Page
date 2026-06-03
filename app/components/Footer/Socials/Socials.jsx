8c661df814cc560694f3fb3ad38a9a8459e374ef
Jacob Diehl <jakediehl17@gmail.com>
2026-06-03 06:46:02 -0600

    fixed the socials icon

--- app/components/Footer/Socials/Socials.jsx
+++ app/components/Footer/Socials/Socials.jsx
@@ -1,38 +1,48 @@
-"use client"
+"use client";
+
 import styles from "./Socials.module.css";
 
-
 export default function SocialIcons({ isSmall = false }) {
-    return (
+  return (
+    <div className={isSmall ? styles.sectionSmall : styles.section}>
+      <a
+        href="https://www.facebook.com/owls.skills/"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Facebook"
+      >
+        <img
+          src="/images/socials/facebook_icon-01.png"
+          alt="Facebook"
+          className={`${styles.socialIcon} ${styles.facebookIcon}`}
+        />
+      </a>
 
-        isSmall ?
+      <a
+        href="https://www.instagram.com/owls.skills/"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Instagram"
+      >
+        <img
+          src="/images/socials/IG.png"
+          alt="Instagram"
+          className={styles.socialIcon}
+        />
+      </a>
 
-            <div className={styles.section_small
-            } >
-                <a href="https://www.facebook.com/owls.skills/" >
-                    <img src="/images/socials/facebook_icon-01.png" alt="facebook" className={`${styles.social_icon} ${styles.size_correction_for_facebook}`} />
-                </a>
-                <a href="https://www.instagram.com/owls.skills/">
-                    <img src="/images/socials/IG.png" alt="Instagram" className={styles.social_icon} />
-                </a>
-                <a href="https://www.youtube.com/channel/UCVF10fU-1uK81GIvE9xSdKg">
-                    <img src="/images/socials/youtube.png" alt="youtube" className={styles.social_icon} />
-                </a>
-            </div >
-            :
-            <div className={styles.section}>
-                <a href="https://www.facebook.com/owls.skills/" >
-                    <img src="/images/socials/facebook_icon-01.png" alt="facebook" className={`${styles.social_icon} ${styles.size_correction_for_facebook}`} />
-                </a>
-                <a href="https://www.instagram.com/owls.skills/">
-                    <img src="/images/socials/IG.png" alt="Instagram" className={styles.social_icon} />
-                </a>
-                <a href="https://www.youtube.com/channel/UCVF10fU-1uK81GIvE9xSdKg">
-                    <img src="/images/socials/youtube.png" alt="youtube" className={styles.social_icon} />
-                </a>
-            </div>
-
-
-    )
+      <a
+        href="https://www.youtube.com/channel/UCVF10fU-1uK81GIvE9xSdKg"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="YouTube"
+      >
+        <img
+          src="/images/socials/youtube.png"
+          alt="YouTube"
+          className={styles.socialIcon}
+        />
+      </a>
+    </div>
+  );
 }
-
